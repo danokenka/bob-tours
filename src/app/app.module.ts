63ace4d19c1226ee6d26de97d1880fec38bc20b3
Daniel Okenka <danokenka@me.com>
2021-05-03 19:45:03 -0400

    finished ionic storage angular with a bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+// to make this work I had to install npm install @ionic/storage@2.0.0 --save --save-exact
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,17 @@ export class FavoritesService {
   public favIDs: Array<number>;
   public favTours: Array<any>;
 
-  constructor() { }
+  constructor(private storage: Storage) { }
 
     initialize(tours) {
-      this.favTours = []
-      this.favIDs = JSON.parse(window.localStorage.getItem('FavoritesIDs'));
-      if (this.favIDs == null) {
-        this.favIDs = []
+      this.favTours = [];
+      // this.favIDs = JSON.parse(window.localStorage.getItem('FavoritesIDs'));
+      this.storage.create().then(() => {
+        this.storage.get('FavoritesIDs').then(ids => {
+          this.favIDs = ids;
+      
+          if (this.favIDs == null) {
+        this.favIDs = [];
       } else {
         tours.forEach(tour => {
           if (this.favIDs.indexOf(tour.ID) != -1) {
@@ -22,19 +27,23 @@ export class FavoritesService {
           }
         });
       }
+    });
+  });
     }
 
     add(tour) {
       this.favIDs.push(tour.ID);
       this.favTours.push(tour);
-      window.localStorage.setItem('FavoritesIDs', JSON.stringify(this.favIDs));
+      // window.localStorage.setItem('FavoritesIDs', JSON.stringify(this.favIDs));
+      this.storage.set('FavoritesIDs', this.favIDs);
     }
 
     remove(tour) {
       let removeIndex:number = this. favIDs.indexOf(tour.ID);
       if (removeIndex != -1) {
         this.favIDs.splice(removeIndex, 1);
-        window.localStorage.setItems('FavoritesIDs', JSON.stringify(this.favIDs));
+        // window.localStorage.setItems('FavoritesIDs', JSON.stringify(this.favIDs));
+        this.storage.set('FavoritesIDs', this.favIDs);
       }
     }
 
